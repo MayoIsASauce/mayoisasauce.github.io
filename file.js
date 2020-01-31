151ff744bd2d6824783b0c30ab44f7d337cf61e6
MayoIsASauce <60394656+MayoIsASauce@users.noreply.github.com>
2020-01-30 19:02:55 -0500

    Update file.js

--- file.js
+++ file.js
@@ -1,4 +1,3 @@
-// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyAXwymLfWdI7b5QygUugtXU-6ZIx_wDJd8",
     authDomain: "integrated-services-1996.firebaseapp.com",
@@ -9,7 +8,6 @@ var firebaseConfig = {
     appId: "1:235339311505:web:03a02addb767650fa59230",
     measurementId: "G-2CZW1GQ76H"
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var db = firebase.firestore();
@@ -18,31 +16,33 @@ var param = new URLSearchParams(window.location.search);
 var data = param.get('code');
 var state = param.get('state');
 
-var dbs = db.collection('auths').doc(state);
-
-dbs.get().then((doc) => {
-    if (!doc.exists) {
-        creation();
-    } else {
-        dbs.delete();
-        creation();
-    }
-}).catch((err) => {
-    console.error(err);
-});
+if (data != null && state != null) {
+    var dbs = db.collection('auths').doc(state);
+    checker();
+} else {
+    console.log("bad url");
+    document.getElementById("display").innerHTML = "Error 404 page not found";
+}
+function checker() {
+    dbs.get().then((doc) => {
+        if (!doc.exists) {
+            creation();
+        } else {
+            dbs.delete();
+            creation();
+        }
+    }).catch((err) => {
+        console.error(err);
+    });
+}
 
 function creation() {
-    if (data != null && state != null) {
-        dbs.set({
-            auth: data
-        }).then(() => {
-            console.log("Successfully logged you may now close this window.");
-            document.getElementById("display").innerHTML = "Success You May Now Close This Window.";
-        }).catch((err) => {
-            console.log(err);
-        });
-    } else {
-        console.log("bad url");
-        document.getElementById("display").innerHTML = "Error 404 page not found";
-    }
+    dbs.set({
+        auth: data
+    }).then(() => {
+        console.log("Successfully logged you may now close this window.");
+        document.getElementById("display").innerHTML = "Success You May Now Close This Window.";
+    }).catch((err) => {
+        console.log(err);
+    });
 }
